a64bbc8c3f360cc809b78adf220a50d103eb4817
Amirali Heidarzadeh <amiraliheidarzadeh0@gmail.com>
2025-04-05 23:54:50 +0330

    updating

--- app.py
+++ app.py
@@ -401,4 +401,6 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # دریافت پورت از متغیر محیطی
+    app.run(host="0.0.0.0", port=port, debug=False)
